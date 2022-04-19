b4d3f72b58f63bdc6bc7bc4d0a2c020426ab3de3
Phil Benson <phil.benson@defra.gov.uk>
2022-04-19 12:22:23 +0100

    Implement with v5.0.0-1

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ import { pipeline, finished } from 'stream'
 import openpgp from 'openpgp'
 import fs from 'fs'
 
-const publicKey = await openpgp.readKey({ armoredKey: fs.readFileSync('public-key.asc', { encoding: 'utf8' }) });
+const publicKeys = await openpgp.readKey({ armoredKey: fs.readFileSync('public-key.asc', { encoding: 'utf8' }) });
 const now = new Date();
 const encryptedFileName = `sample_data_encrypted.json`;
 const fileToEncrypt = 'sample_data.json';
 const stream = fs.createReadStream(fileToEncrypt);
 const encrypted = await openpgp.encrypt({
-  message: await openpgp.createMessage({ text: stream }),
-  encryptionKeys: publicKey
+  message: await openpgp.Message.fromText(stream),
+  publicKeys
 });
 const ws = fs.createWriteStream(`${fileToEncrypt}.enc`);
 pipeline(encrypted, ws, (err, val) => {
+  // this is called
   console.log('file written', err, val)
 });
-// finished(encrypted, (err) => { // this won't be called
-finished(encrypted, {readable: false}, (err) => { // this will be called
+finished(encrypted, (err) => { // this will be called
+// finished(encrypted, {readable: false}, (err) => { // and so will this
   if (!err) {
     console.log('encrypted finished writing without failure')
   } else {
